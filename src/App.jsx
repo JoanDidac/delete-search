7d3f2cbab7f5bf824ab3af8e4851317478cec752
Joan D <joandidachr@gmail.comm>
2023-03-03 11:32:36 +0100

    Adds delete button for ONLY ONE product

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,17 @@ import ProductCart from './components/ProductCard';
 const App = () => {
   const [products, setProducts] = useState(productData);
 
-const handleDelete = () => {
-    setProducts(prevProducts => prevProducts.filter(({id}) => id !== products.id))}
+  // console.log(products);
+  // console.log(handleDelete);
+
+const handleDelete = (deletionId) => {
+    const deleteProducts = (deletionId) => {
+    setProducts(prevProducts => prevProducts.filter(product => product._id !== deletionId)); //handleDelete pilla el id que invoca deleteProduct con esa id. deleteProdcut coge deletionId para filtrar el producto con esa id del products array,al final setProducts es llamada con el array filtrado dentro de la funcion deleteProduct.
+
+  }
+  deleteProducts(deletionId)
+}
+
       
   
 
@@ -16,11 +25,11 @@ const handleDelete = () => {
       <h1>My shopping cart</h1>
       <div className="cart">
      
-        {products.map((elem) => { return <ProductCart product={elem} key={elem._id}  src={products.image}/>
-         
+        {products.map((elem) => { return <ProductCart product={elem} key={elem._id}  src={elem.image} handleDelete= {()=> handleDelete(elem._id)}/> //map itera sobre cada product en el array para pintar ProductCard individualmente. handleDelete PROP!!! se pasa como funcion que invoce handleDelete con el elem.id como parametro!!!
+        
    })
   }
-
+  
       </div>
     </>
   );
